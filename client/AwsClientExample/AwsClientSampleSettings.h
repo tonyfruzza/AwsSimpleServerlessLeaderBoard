572e6e93aae56fe76069851b4affeba33fdc02d8
x254378 <dconnery@onica.com>
2021-04-07 10:52:42 -0500

    created header file containing region

---
+++ client/AwsClientExample/AwsClientSampleSettings.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <aws/core/Region.h>
+
+const char* REGION = Aws::Region::US_WEST_2;
--- client/AwsClientExample/main.cpp
+++ client/AwsClientExample/main.cpp
@@ -11,69 +11,33 @@
 #include <chrono>
 #include <stdlib.h>
 #include <aws/core/Aws.h>
-#include <aws/monitoring/CloudWatchClient.h>
-#include <aws/monitoring/model/PutMetricDataRequest.h>
+#include <aws/core/utils/logging/ConsoleLogSystem.h>
+#include <aws/core/utils/logging/AWSLogging.h>
+#include <aws/core/utils/Outcome.h>
+#include <aws/apigatewayv2/ApiGatewayV2Client.h>
 
 
 using namespace std;
 
-class MetricMaker {
-public:
-    // Based on AWS example
-    // https://docs.aws.amazon.com/sdk-for-cpp/v1/developer-guide/examples-cloudwatch-publish-custom-metrics.html
-    bool publish_ccu_metric(int count){
-        bool ret;
-        Aws::SDKOptions options;
-        
-        Aws::InitAPI(options);
-        {
-            Aws::CloudWatch::CloudWatchClient cw;
-            Aws::CloudWatch::Model::Dimension dimension;
-            Aws::CloudWatch::Model::MetricDatum datum;
-            Aws::CloudWatch::Model::PutMetricDataRequest request;
+const char USER_NAME[] = "tester";
+const char PASSSWORD[] = "testing123";
 
-            dimension.SetName("map");
-            dimension.SetValue("capture_the_flag");
+#include "AwsClientSampleSettings.h"
 
-            datum.SetMetricName("CCU");
-            datum.SetUnit(Aws::CloudWatch::Model::StandardUnit::None);
-            datum.SetValue(count);
-            datum.AddDimensions(dimension);
-            
-            request.SetNamespace("dev_gameoftheyear");
-            request.AddMetricData(datum);
+int main(int argc, char** argv) {
+    Aws::SDKOptions options;
 
-            auto outcome = cw.PutMetricData(request);
-            if (!outcome.IsSuccess())
-            {
-                cout << "Failed to put sample metric data:" <<
-                    outcome.GetError().GetMessage() << endl;
-                ret = true;
-            }
-            else
-            {
-                cout << "CCU metric set to: " << count << endl;
-                ret = false;
-            }
-        }
-        Aws::ShutdownAPI(options);
-        return ret;
-    }
-};
+    Aws::Utils::Logging::LogLevel logLevel{ Aws::Utils::Logging::LogLevel::Error };
+    options.loggingOptions.logger_create_fn = [logLevel] {return make_shared<Aws::Utils::Logging::ConsoleLogSystem>(logLevel); };
 
+    Aws::InitAPI(options);
 
-void collect_user_count(){
-    srand((long)time(NULL));
-    MetricMaker mm;
-    while(1){
-        mm.publish_ccu_metric(rand() % 20 + 1);
-        this_thread::sleep_for(chrono::seconds(5));
-    }
-}
+    Aws::Client::ClientConfiguration clientConfig;
+    client.region = REGION;    // region must be set for Cognito operations
+    Aws::ApiGatewayV2::ApiGatewayV2Client(clientConfig)
 
+    int exitStatus = RunMainLoop();
 
-int main(int argc, const char * argv[]) {
-    thread t_handler(collect_user_count);
-    t_handler.join();
+    Aws::ShutdownAPI(options);
     return 0;
 }
