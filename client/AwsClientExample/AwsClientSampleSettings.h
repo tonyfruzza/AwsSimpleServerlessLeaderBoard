026b08903caff093f999b6fe567d42eb677c2628
Tony Fruzza <tony240zt@gmail.com>
2021-04-07 17:41:08 -0700

    No AWS API required.

--- client/AwsClientExample/AwsClientSampleSettings.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <aws/core/Region.h>
-
-const char* REGION = Aws::Region::US_WEST_2;
--- client/AwsClientExample/main.cpp
+++ client/AwsClientExample/main.cpp
@@ -1,41 +1,143 @@
-//
-//  main.cpp
-//  AwsClientExample
-//
-//  Created by Daniel Connery on 4/05/21.
-//
-
 #include <iostream>
-#include <thread>
+#include <string>
+#include <sstream>
 #include <time.h>
-#include <chrono>
 #include <stdlib.h>
-#include <aws/core/Aws.h>
-#include <aws/core/utils/logging/ConsoleLogSystem.h>
-#include <aws/core/utils/logging/AWSLogging.h>
-#include <aws/core/utils/Outcome.h>
-#include <aws/apigatewayv2/ApiGatewayV2Client.h>
 
+#include <curl/curl.h>
+
+/*
+    JSON lib used: https://github.com/nlohmann/json/tree/v3.9.1
+    For OS X install JSON lib
+    brew tap nlohmann/json
+    brew install nlohmann-json
+
+*/
+#include <nlohmann/json.hpp>
 
 using namespace std;
 
-const char USER_NAME[] = "tester";
-const char PASSSWORD[] = "testing123";
+const string USER_NAME ("tester");
+const string PASSSWORD ("testing123");
+const string APIGW_ENDPOINT ("sn4h6yk8gg.execute-api.us-west-2.amazonaws.com/dev");
+static string readBuffer;
+static string auth_header;
 
-#include "AwsClientSampleSettings.h"
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    size_t realsize = size * nmemb;
+    readBuffer.append((char *)contents, realsize);
+    return realsize;
+}
+
+int sign_in(string user, string password){
+    CURL *curl;
+    CURLcode res;
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+    int return_val = 1;
+    nlohmann::json j_request = {
+        {"username", USER_NAME},
+        {"password", PASSSWORD}
+    };
+    string login_request = j_request.dump();
+    if(curl){
+        string url = "https://" + APIGW_ENDPOINT + "/create_user";
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, login_request.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK){
+            return_val = 0;
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                  curl_easy_strerror(res));
+        }
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+    return return_val;
+}
+
+int add_high_score(string game_session_id, int score){
+    CURL *curl;
+    CURLcode res;
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+    struct curl_slist *list = NULL;
+    int return_val = 1;
+    
+    nlohmann::json j_request = {
+        {"username", USER_NAME},
+        {"game_id", game_session_id},
+        {"score", score}
+    };
+    
+    string score_request = j_request.dump();
+    if(curl){
+        string url = "https://" + APIGW_ENDPOINT + "/put_highscore";
+        list = curl_slist_append(list, auth_header.c_str());
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, score_request.c_str());
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK){
+            return_val = 0;
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                  curl_easy_strerror(res));
+        }
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+    return return_val;
+}
+
+int get_high_scores(){
+    CURL *curl;
+    CURLcode res;
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+    struct curl_slist *list = NULL;
+    int return_val = 1;
+    
+    if(curl){
+        string url = "https://" + APIGW_ENDPOINT + "/get_highscore";
+        list = curl_slist_append(list, auth_header.c_str());
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        res = curl_easy_perform(curl);
+        if(res != CURLE_OK){
+            return_val = 0;
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                  curl_easy_strerror(res));
+        }
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+    return return_val;
+}
 
 int main(int argc, char** argv) {
-    Aws::SDKOptions options;
-
-    Aws::Utils::Logging::LogLevel logLevel{ Aws::Utils::Logging::LogLevel::Error };
-    options.loggingOptions.logger_create_fn = [logLevel] {return make_shared<Aws::Utils::Logging::ConsoleLogSystem>(logLevel); };
-
-    Aws::InitAPI(options);
-
-    Aws::Client::ClientConfiguration clientConfig;
-    clientConfig.region = REGION;    // region must be set for Cognito operations
-    Aws::ApiGatewayV2::ApiGatewayV2Client apigwClient(clientConfig);
-
-    Aws::ShutdownAPI(options);
+    srand((int)time(NULL));
+    
+    sign_in("tester", "testing321");
+    using json = nlohmann::json;
+    json j = json::parse(readBuffer);
+    if(j["auth"].get<std::string>().compare("success") != 0){
+        cout << "Error: Authenication failure." << endl;
+        return -1;
+    }
+    cout << "Authenticated with user: '" << USER_NAME << "'" << endl;
+    auth_header = "Authorization: " + j["msg"].get<std::string>();
+    stringstream ss;
+    ss << "bestgame_" << rand() % 1000;
+    string game_id = ss.str();
+    int new_high_score = rand() % 2400 + 1;
+    cout << "Adding new high score of: " << new_high_score << endl;
+    add_high_score(game_id, new_high_score);
+    ss.str(string()); // clear this stringstream for reuse.
+    readBuffer = "";
+    get_high_scores();
+    cout << "High score values: " << readBuffer << endl;
     return 0;
 }
